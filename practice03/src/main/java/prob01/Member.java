56a50393a9cef843e0f24fb685a826193c73e616
BIT_R41 <BIT_R41@DESKTOP-2ORHRMC>
2021-04-28 11:52:43 +0900

    객체지향 프로그래밍 필드

--- practice03/src/main/java/prob01/Member.java
+++ practice03/src/main/java/prob01/Member.java
@@ -1,4 +1,32 @@
 package prob01;
 
 public class Member {
+
+	private String id;
+	private String name;
+	private int point;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public int getPoint() {
+		return this.point;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return this.id;
+	}
 }
