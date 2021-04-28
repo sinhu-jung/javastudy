fcc18f20c39885138d3dfb9827d259610f0b4626
BIT_R41 <BIT_R41@DESKTOP-2ORHRMC>
2021-04-29 08:56:12 +0900

    practice04 prob01

---
+++ practice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,21 @@
+package prob01;
+
+public class Printer {
+	
+	public void println(int inum) {
+		System.out.println(inum);
+	}
+	
+	public void println(double dnum) {
+		System.out.println(dnum);
+	}
+	
+	public void println(boolean tf) {
+		System.out.println(tf);
+	}
+	
+	public void println(String name) {
+		System.out.println(name);
+	}
+
+}
