2db39b0994d3d75b57e97cef97eac97dffd5a448
BIT_R41 <BIT_R41@DESKTOP-2ORHRMC>
2021-04-29 16:44:55 +0900

    practice04 prob03 추상 클래스 이용

--- practice04/src/main/java/prob03/Marine.java
+++ practice04/src/main/java/prob03/Marine.java
@@ -5,11 +5,11 @@ public class Marine extends Unit{
 	private int x;
 	private int y;
 
-	void move(int x, int y) {
+	public void move(int x, int y) {
 		/* 지정된 위치로 이동 */
 	}
 
-	void stop() {
+	public void stop() {
 		/* 현재 위치에 정지 */
 	}
 	
--- practice04/src/main/java/prob03/Unit.java
+++ practice04/src/main/java/prob03/Unit.java
@@ -1,14 +1,9 @@
 package prob03;
 
-public class Unit {
+public abstract class Unit {
 	
-	void move(int x, int y) {
-		/* 지정된 위치로 이동 */
-	}
-
-	void stop() {
-		/* 현재 위치에 정지 */
-	}
+	public abstract void move(int x, int y);
+	public abstract void stop();
 	
 
 }
