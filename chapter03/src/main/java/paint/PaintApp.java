58a76afde3524e3d9864fd3c1b5c3b5a36ee764d
BIT_R41 <BIT_R41@DESKTOP-2ORHRMC>
2021-04-29 09:55:29 +0900

    객체지향프로그래밍(상속/생성자/오버라이딩)

--- chapter03/src/main/java/paint/PaintApp.java
+++ chapter03/src/main/java/paint/PaintApp.java
@@ -14,12 +14,39 @@ public class PaintApp {
 		
 		point1.show(false);
 		
-		Point point3 = new ColorPoint();
-		point3.setX(50);
-		point3.setY(100);
-		((ColorPoint)point3).setColor("red");
+		Point point3 = new ColorPoint(50, 100, "red");
 		point3.show();
 		point3.show(true);
+		drawPoint(point3);
+		
+		Rect rect = new Rect();
+		drawShape(rect);
+		
+		Triangle triangle = new Triangle();
+		drawShape(triangle);
+		
+		Circle circle = new Circle();
+		drawShape(circle);
 	}
+	
+//	public static void drawColorPoint(ColorPoint pt) {
+//		pt.show();
+//	}
+	
+	public static void drawPoint(Point pt) {
+		pt.show();
+	}
+
+	public static void drawShape(Shape shape) {
+		shape.draw();
+	}
+	
+//	public static void drawRect(Rect rect) {
+//		rect.draw();
+//	}
+//	
+//	public static void drawTriangle(Triangle triangle) {
+//		triangle.draw();
+//	}
 
 }
