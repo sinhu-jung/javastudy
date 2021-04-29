f51c9c14b6e6e96cf9b8bc6a8167f8ac3c180985
BIT_R41 <BIT_R41@DESKTOP-2ORHRMC>
2021-04-29 13:26:57 +0900

    Exception

---
+++ chapter03/src/main/java/exception/ExceptionTest.java
@@ -0,0 +1,31 @@
+package exception;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 10 - a;
+		
+		System.out.println("some codes...1");
+		try {
+			System.out.println("some codes...2");
+			int result = (1 + 2 + 3) / b;
+			System.out.println("some codes...3");
+		}catch(ArithmeticException e) {
+			/* 예외 처리 */
+			// 1. 사과(프로그램 사용자에게)
+			System.out.println("미안합니다...");
+			// 2. 로깅(기록 남겨놓기(파일 또는 db에))
+			System.out.println("error:" + e);
+			// 3. 정상 종료
+			return;
+		}finally {
+			System.out.println("some codes...4");
+		}
+		
+		System.out.println("some codes...5");
+		
+	}
+
+}
