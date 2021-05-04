9aa81715bf526ded0b86c2e72ae2c6c775dc07bb
BIT_R41 <BIT_R41@DESKTOP-2ORHRMC>
2021-05-04 13:31:11 +0900

    TCPClient/Server

--- chapter04/src/main/java/network/test/TCPServer.java
+++ chapter04/src/main/java/network/test/TCPServer.java
@@ -37,6 +37,7 @@ public class TCPServer {
 				OutputStream os = socket.getOutputStream();
 				
 				while(true) {
+					// 4. 데이터 읽기
 					byte[] buffer = new byte[256];
 					int readByteCount = is.read(buffer); // blocking
 					
@@ -49,8 +50,15 @@ public class TCPServer {
 					
 					String data = new String(buffer, 0, readByteCount, "utf-8");
 					System.out.println("[server] received: " + data);
+					
+					// 5. 데이터 쓰기
+					os.write(data.getBytes("utf-8"));
 				}
-			} catch(IOException e) {
+			} catch(SocketException e) {
+				System.out.println("[server] suddenly closed by client");
+			}
+			
+			catch(IOException e) {
 				e.printStackTrace();
 			} finally {
 				try {
