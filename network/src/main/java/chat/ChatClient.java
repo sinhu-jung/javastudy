662962fc93e73bee65eb24d1f8025c5e35b4141c
BIT_R41 <BIT_R41@DESKTOP-2ORHRMC>
2021-05-06 14:16:27 +0900

    chat prob

---
+++ network/src/main/java/chat/ChatClient.java
@@ -0,0 +1,9 @@
+package chat;
+
+public class ChatClient {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ network/src/main/java/chat/ChatClientThread.java
@@ -0,0 +1,7 @@
+package chat;
+
+import java.io.BufferedReader;
+
+public class ChatClientThread extends Thread {
+	private BufferedReader br;
+}
