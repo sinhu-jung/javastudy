890a95f5fae21f9a15eb393941b2adccc80934b5
BIT_R41 <BIT_R41@DESKTOP-2ORHRMC>
2021-05-07 10:23:22 +0900

    socket options

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -16,6 +16,10 @@ public class TCPServer {
 			// 1. 서버소켓 생성
 			serverSocket = new ServerSocket();
 			
+			// 1-1. Time-Wait 상태에서도 소켓에 포트 번호
+			// 		할당이 가능하게 하기 위해서...
+			serverSocket.setReuseAddress(true);
+			
 			// 2. 바인딩(Binding)
 			//    Socket에 InetSocketAddress (IPAddress + Port)
 			//    IPAddress : 0.0.0.0 ( 모든 ip 연결 허용)			
@@ -52,7 +56,13 @@ public class TCPServer {
 					System.out.println("[server] received: " + data);
 					
 					// 6. 데이터 쓰기
-					os.write(data.getBytes("utf-8"));
+					try {
+						Thread.sleep(2000);
+						os.write(data.getBytes("utf-8"));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					
 				}
 			} catch(SocketException e) {
 				System.out.println("[server] suddenly closed by client");
